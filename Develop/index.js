beefa0005b4605420b0f409b69ade44bf912c2a7
Sonarie <sonarie1980@gmail.com>
2022-02-27 20:25:14 -0600

    user prompts produce answers - no errors

--- Develop/index.js
+++ Develop/index.js
@@ -1,96 +1,78 @@
 // TODO: Include packages needed for this application
 var inquirer = require("inquirer");
 const fs = require("fs");
-const util = require('util');
-
-const writeFileAsync = util.promisify(fs.writeFile);
-
+const generateMarkdown = require("./utils/generateMarkdown");
+var toc = require("markdown-toc");
 
 // TODO: Create an array of questions for user input
-const questions = () => {
+// const questions = () => {
+
 inquirer
   .prompt([
     {
       type: "input",
-      name: "title",
+      name: "Title",
       message: "What is the title of your project?",
     },
     {
       type: "input",
-      name: "description",
+      name: "Description",
       message: "Please provide a description of your project.",
     },
     {
       type: "input",
-      name: "intallation",
+      name: "Table of Contents",
+      message: "Please provide a table of contents",
+    },
+    {
+      type: "input",
+      name: "Intallation",
       message: "How will the user install your project?",
     },
     {
       type: "input",
-      name: "usage",
+      name: "Usage",
       message: "How will your project be used?",
     },
     {
       type: "input",
-      name: "license",
+      name: "License",
       message: "Please provide the licensing information",
     },
     {
       type: "input",
-      name: "contributing",
+      name: "Contributing",
       message: "Who is contributing to your project.",
     },
     {
       type: "input",
-      name: "tests",
+      name: "Tests",
       message: "Which tests you would like to include?",
     },
     {
       type: "input",
-      name: "questions",
+      name: "Questions",
       message: "Are there any FAQ's?",
     },
-  ])};
+  ])
 
+  // TODO: Create a function to write README file
+  //function writeToFile(fileName, data) {}
+  .then((answers) => {
+    fs.writeFile(generateMarkdown(answers), "./utils/generateMarkdown.js"),
+      then(
+        fs.copyFileSync(
+          generateMarkdown(answers),
+          "./utils/generateMarkdown.js"
+        )
+      );
+  })
 
-
-function generateMarkdown(data){
-  return`# ${data.title}
-  ${badge}
-  ${data.Description}
-  ## Table of Contents:
-  * [Installation](#installation)
-  * [Usage](#usage)
-  * [License](#license)
-  * [Contributing](#contributing)
-  * [Tests](#tests)
-  * [Questions](*questions)
-  ### Installation:
-  Here is a step by step guide to installing this project
-  ${data.installation}
-  ### Usage: ${data.usage} 
-  ### License:
-  This project is licensed by:
-  ${data.license}
-  ## Contributing:
-  ${data.contributing}
-  ## Tests:
-  ${data.tests}
-  ## Questions:
-  ${data.questions}
-
-  `
-}
-
-questions()
-.then(data) => writeFileAsync('generateMarkdown.js', generateMD(data))
-.then(() => console.log('Sucessfully generated to README'))
-.catch((err) => console.error(err));
-
-
-// TODO: Create a function to write README file
-//function writeToFile(fileName, data) {}
-
+  .catch((error) => {
+    if (error.isTtyError) {
+      console.log("Something went wrong, file not generated.");
+    }
+  });
 // TODO: Create a function to initialize app
 //function init() {}
 
